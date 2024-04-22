aaf805e16da847360869e7da22eb066e79afa513
xianlinfeng <xianlinfeng@holderzone.cn>
2024-04-22 13:19:19 +0800

    增加数据库驱动接口

--- golang/mbpb/v1/shouldBind.go
+++ golang/mbpb/v1/shouldBind.go
@@ -5,7 +5,38 @@ import (
 	"fmt"
 
 	"github.com/bytedance/sonic"
+	"google.golang.org/protobuf/encoding/protojson"
 )
+
+// DBDetail
+func (req *EnableRequest) Scan(value interface{}) error {
+
+	if value == nil {
+		return nil
+	}
+
+	b, ok := value.([]byte)
+	if !ok {
+		return fmt.Errorf("value is not []byte, value: %v", value)
+	}
+
+	// return json.Unmarshal(b, &db)
+	return protojson.Unmarshal(b, &req)
+}
+
+// Value Valuer
+func (req *EnableRequest) Value() (driver.Value, error) {
+
+	if req == nil {
+		return nil, nil
+	}
+
+	buff, err := protojson.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+	return string(buff), nil
+}
 
 // DBDetail
 func (db *DBDetail) Scan(value interface{}) error {
