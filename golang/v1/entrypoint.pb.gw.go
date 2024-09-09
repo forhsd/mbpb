f8d6d6a50508f8feb68a243fecf872716c21d454
xianlinfeng <xianlinfeng@holderzone.cn>
2024-09-09 14:18:44 +0800

    add runstatus enum

--- golang/v1/entrypoint.pb.gw.go
+++ golang/v1/entrypoint.pb.gw.go
@@ -135,6 +135,7 @@ func request_MBetl_GetTaskflowStatus_0(ctx context.Context, marshaler runtime.Ma
 // UnaryRPC     :call MBetlServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterMBetlHandlerFromEndpoint instead.
+// GRPC interceptors will not work for this type of registration. To use interceptors, you must use the "runtime.WithMiddlewares" option in the "runtime.NewServeMux" call.
 func RegisterMBetlHandlerServer(ctx context.Context, mux *runtime.ServeMux, server MBetlServer) error {
 
 	mux.Handle("GET", pattern_MBetl_DataLineage_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
@@ -232,7 +233,7 @@ func RegisterMBetlHandler(ctx context.Context, mux *runtime.ServeMux, conn *grpc
 // to "mux". The handlers forward requests to the grpc endpoint over the given implementation of "MBetlClient".
 // Note: the gRPC framework executes interceptors within the gRPC handler. If the passed in "MBetlClient"
 // doesn't go through the normal gRPC flow (creating a gRPC client etc.) then it will be up to the passed in
-// "MBetlClient" to call the correct interceptors.
+// "MBetlClient" to call the correct interceptors. This client ignores the HTTP middlewares.
 func RegisterMBetlHandlerClient(ctx context.Context, mux *runtime.ServeMux, client MBetlClient) error {
 
 	mux.Handle("GET", pattern_MBetl_DataLineage_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
